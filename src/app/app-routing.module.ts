8ebea6a751e169b6f207b8b7c0723c952bc53a8f
IdrisOmit <saddi.idris@gmail.com>
2024-11-14 12:00:57 +0100

    add TTC Calculator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,12 @@ import { AddCvComponent } from "./cv/add-cv/add-cv.component";
 import { CvComponent } from "./cv/cv/cv.component";
 import { DetailsCvComponent } from "./cv/details-cv/details-cv.component";
 import { RhComponent } from "./optimizationPattern/rh/rh.component";
+import { TtcCalculatorComponent } from "./components/ttc-calculator/ttc-calculator.component";
 
 const routes: Route[] = [
   { path: "login", component: LoginComponent },
   { path: "rh", component: RhComponent },
+  { path: "ttc", component: TtcCalculatorComponent },
   {
     path: "cv",
     component: CvComponent,
---
+++ src/app/components/ttc-calculator/ttc-calculator.component.ts
@@ -0,0 +1,46 @@
+import { Component, signal, computed } from '@angular/core';
+@Component({
+  selector: 'app-ttccalculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './ttc-calculator.component.html',
+  styleUrl: './ttc-calculator.component.css',
+})
+export class TtcCalculatorComponent {
+  price = signal<number>(0);
+  quantity = signal<number>(1);
+  tva = signal<number>(18);
+
+  unitPrice = computed(() => {
+    const rawUnitPrice = this.price() + (this.price() * this.tva()) / 100;
+    return rawUnitPrice.toFixed(3);
+  });
+  
+  totalPrice = computed(() => {
+    const unitPrice = parseFloat(this.unitPrice());
+    const disc = this.discount();
+    const rawTotalPrice = this.quantity() * unitPrice * (1 - disc / 100);
+    return rawTotalPrice.toFixed(3);
+  });
+  
+  discount = computed(() => {
+    if (this.quantity() > 15) return 30;
+    if (this.quantity() > 10) return 20;
+    return 0;
+  });
+
+  onTvaChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.tva.set(input.valueAsNumber || 0);
+  }
+
+  onPriceChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.price.set(input.valueAsNumber || 0);
+  }
+
+  onQuantityChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.quantity.set(input.valueAsNumber || 0);
+  }
+}
