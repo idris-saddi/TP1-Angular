244827ffdb73f34dd51f79c7fbc93ee8d7b1b47d
IdrisOmit <saddi.idris@gmail.com>
2024-11-22 15:42:35 +0100

    try to implement lazy loading to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,63 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Route } from "@angular/router";
-import { TodoComponent } from "./todo/todo/todo.component";
-import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
-import { ColorComponent } from "./components/color/color.component";
-import { FrontComponent } from "./templates/front/front.component";
-import { AdminComponent } from "./templates/admin/admin.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { NF404Component } from "./components/nf404/nf404.component";
-import { AuthGuard } from "./auth/guards/auth.guard";
-import { AddCvComponent } from "./cv/add-cv/add-cv.component";
-import { CvComponent } from "./cv/cv/cv.component";
-import { DetailsCvComponent } from "./cv/details-cv/details-cv.component";
-import { RhComponent } from "./optimizationPattern/rh/rh.component";
-import { TtcCalculatorComponent } from "./components/ttc-calculator/ttc-calculator.component";
+import { RouterModule} from "@angular/router";
+import { Routes } from "@angular/router";
 
-const routes: Route[] = [
-  { path: "login", component: LoginComponent },
-  { path: "rh", component: RhComponent },
-  { path: "ttc", component: TtcCalculatorComponent },
+// Define the routes
+export const routes: Routes = [
   {
-    path: "cv",
-    component: CvComponent,
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent),
   },
-  { path: "cv/add", component: AddCvComponent, canActivate: [AuthGuard] },
-  { path: "cv/:id", component: DetailsCvComponent },
   {
-    path: "",
-    component: FrontComponent,
+    path: 'cv',
+    loadComponent: () => import('./cv/cv/cv.component').then(m => m.CvComponent),
     children: [
-      { path: "todo", component: TodoComponent },
-      { path: "word", component: MiniWordComponent },
+      {
+        path: 'add',
+        loadComponent: () => import('./cv/add-cv/add-cv.component').then(m => m.AddCvComponent), // Lazy load AddCvComponent
+      },
+      {
+        path: ':id',
+        loadComponent: () => import('./cv/details-cv/details-cv.component').then(m => m.DetailsCvComponent), // Lazy load DetailsCvComponent
+      },
     ],
   },
   {
-    path: "admin",
-    component: AdminComponent,
-    children: [{ path: "color", component: ColorComponent }],
+    path: 'rh',
+    loadComponent: () => import('./optimizationPattern/rh/rh.component').then(m => m.RhComponent),
   },
-  { path: "**", component: NF404Component },
+  {
+    path: 'ttc',
+    loadComponent: () => import('./components/ttc-calculator/ttc-calculator.component').then(m => m.TtcCalculatorComponent),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./templates/front/front.component').then(m => m.FrontComponent),
+    children: [
+      {
+        path: 'todo',
+        loadComponent: () => import('./todo/todo/todo.component').then(m => m.TodoComponent),
+      },
+      {
+        path: 'word',
+        loadComponent: () => import('./directives/mini-word/mini-word.component').then(m => m.MiniWordComponent),
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./templates/admin/admin.component').then(m => m.AdminComponent),
+    children: [
+      {
+        path: 'color',
+        loadComponent: () => import('./components/color/color.component').then(m => m.ColorComponent),
+      },
+    ],
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./components/nf404/nf404.component').then(m => m.NF404Component),
+  },
 ];
 
 @NgModule({
