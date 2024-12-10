2d85f12ab76cc3c77095c800d03da93c14e69a03
IdrisOmit <saddi.idris@gmail.com>
2024-12-10 23:20:05 +0100

    update rh page

--- src/app/optimizationPattern/rh/rh.component.ts
+++ src/app/optimizationPattern/rh/rh.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, inject } from '@angular/core';
-import {User, UsersService} from "../users.service";
+import { User, UsersService } from "../users.service";
 import * as ChartJs from 'chart.js/auto';
 import { UserListComponent } from '../user-list/user-list.component';
+
 @Component({
-    selector: 'app-rh',
-    templateUrl: './rh.component.html',
-    styleUrls: ['./rh.component.css'],
-    standalone: true,
-    imports: [UserListComponent]
+  selector: 'app-rh',
+  templateUrl: './rh.component.html',
+  styleUrls: ['./rh.component.css'],
+  standalone: true,
+  imports: [UserListComponent]
 })
 export class RhComponent implements OnInit {
   private userService = inject(UsersService);
@@ -16,36 +17,45 @@ export class RhComponent implements OnInit {
   evenUsers: User[];
   chart: any;
 
-  /** Inserted by Angular inject() migration for backwards compatibility */
-  constructor(...args: unknown[]);
   constructor() {
     this.oddUsers = this.userService.getOddOrEven(true);
     this.evenUsers = this.userService.getOddOrEven();
   }
 
   ngOnInit(): void {
-        this.createChart();
-    }
-  addUser(list: User[], newUser: string) {
-    this.userService.addUser(list, newUser);
+    this.createChart();
   }
-  createChart(){
+
+  addUser(list: User[], newUser: string) {
+    // Add the user to the correct list
+    this.userService.addUser(list, newUser);
+
+    // After adding the user, update the chart data
+    if (list === this.oddUsers) {
+      this.chart.data.datasets[0].data[0] = this.oddUsers.length; // Update workers count
+    } else if (list === this.evenUsers) {
+      this.chart.data.datasets[0].data[1] = this.evenUsers.length; // Update boss count
+    }
+
+    // Update the chart
+    this.chart.update();
+  }
+
+  createChart() {
     const data = [
       { users: 'Workers', count: this.oddUsers.length },
       { users: 'Boss', count: this.evenUsers.length },
     ];
-    this.chart = new ChartJs.Chart("MyChart",
-    {
+
+    this.chart = new ChartJs.Chart("MyChart", {
       type: 'bar',
-        data: {
-          labels: data.map(row => row.users),
-        datasets: [
-        {
+      data: {
+        labels: data.map(row => row.users),
+        datasets: [{
           label: 'Entreprise stats',
-          data: data.map(row => row.count)
-        }
-      ]
-    }
+          data: data.map(row => row.count),
+        }]
+      }
     });
   }
 }
