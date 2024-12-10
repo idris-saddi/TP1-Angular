8239a311abeb690e42009ded737ea08ac6940cea
Zied <ziedmaghraoui58@gmail.com>
2024-12-11 00:51:42 +0100

    calculate fibonnaci outside angular

--- src/app/optimizationPattern/user-list/user-list.component.ts
+++ src/app/optimizationPattern/user-list/user-list.component.ts
@@ -1,29 +1,37 @@
-import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from '@angular/core';
-import {User} from "../users.service";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  NgZone,
+  inject,
+} from '@angular/core';
+import { User } from '../users.service';
 import { FormsModule } from '@angular/forms';
-import Memo from 'memo-decorator'
+import Memo from 'memo-decorator';
 
 export const fibonnaci = (n: number): number => {
-  if (n==1 || n==0) {
+  if (n == 1 || n == 0) {
     return 1;
   }
-  return fibonnaci(n-1) + fibonnaci(n-2);
-}
+  return fibonnaci(n - 1) + fibonnaci(n - 2);
+};
 
 @Component({
-    selector: 'app-user-list',
-    templateUrl: './user-list.component.html',
-    styleUrls: ['./user-list.component.css'],
-    standalone: true,
-    imports: [FormsModule],
-    changeDetection:ChangeDetectionStrategy.OnPush //changes here trigger unecessary checks for rh 
-    //only run change detection whe input ref changes or event emitted
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+  standalone: true,
+  imports: [FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserListComponent {
   @Input() usersCluster: string = '';
   @Input() users: User[] = [];
   @Output() add = new EventEmitter<string>();
   userFullName: string = '';
+  private ngZone = inject(NgZone);
 
   addUser() {
     this.add.emit(this.userFullName);
@@ -31,8 +39,11 @@ export class UserListComponent {
   }
   @Memo() // caches res of fibo for same n will return the cached result without recalculating !!
   fibo(n: number): number {
-    const fib = fibonnaci(n);
-    console.log({n, fib});
+    let fib = 0;
+    this.ngZone.runOutsideAngular(() => {
+      fib = fibonnaci(n);
+    });
+    console.log({ n, fib });
     return fib;
   }
 }
