c62b1162b5a3e434c4b3d614d68bfba83ec70145
IdrisOmit <saddi.idris@gmail.com>
2024-12-10 23:10:00 +0100

    update fib fund

--- src/app/optimizationPattern/user-list/user-list.component.ts
+++ src/app/optimizationPattern/user-list/user-list.component.ts
@@ -1,35 +1,44 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {User} from "../users.service";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { User } from '../users.service';
 import { FormsModule } from '@angular/forms';
 
-
-export const fibonnaci = (n: number): number => {
-  if (n==1 || n==0) {
-    return 1;
-  }
-  return fibonnaci(n-1) + fibonnaci(n-2);
-}
-
 @Component({
-    selector: 'app-user-list',
-    templateUrl: './user-list.component.html',
-    styleUrls: ['./user-list.component.css'],
-    standalone: true,
-    imports: [FormsModule]
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+  standalone: true,
+  imports: [FormsModule]
 })
 export class UserListComponent {
   @Input() usersCluster: string = '';
   @Input() users: User[] = [];
   @Output() add = new EventEmitter<string>();
   userFullName: string = '';
+  
+  // Memoization cache for Fibonacci
+  private fibCache: { [key: number]: number } = {};
+
+  // Optimized Fibonacci function with memoization
+  fibo(n: number): number {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+
+    // Check if the value is already in the cache
+    if (this.fibCache[n] !== undefined) {
+      return this.fibCache[n];
+    }
+
+    // Otherwise, calculate and store it in the cache
+    this.fibCache[n] = this.fibo(n - 1) + this.fibo(n - 2);
+    console.log({ n, fib: this.fibCache[n] });
+
+    return this.fibCache[n];
+  }
+
+  // Function to add a user
   addUser() {
     this.add.emit(this.userFullName);
     this.userFullName = '';
-  }
-  fibo(n: number): number {
-    const fib = fibonnaci(n);
-    console.log({n, fib});
-
-    return fib;
   }
 }
